09e704b0d20c886bbe5284b4c9bed67c508dddee
Montazer Malik <mntthr1122@gmail.com>
2022-05-05 18:42:13 +0300

    Update newUpdate.php

--- app/Console/Commands/newUpdate.php
+++ app/Console/Commands/newUpdate.php
@@ -4,6 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Artisan;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+//use git command in terminal
 
 
 class newUpdate extends Command
@@ -29,25 +32,26 @@ class newUpdate extends Command
      */
     public function handle()
     {
-        // Artisan::call('migrate');
-        // $this->info('Migrated.');
-        // Artisan::call('git merge main');
-        // //artisan to merge the main branch
-        // $this->info('Merged.');
-        // return 0;
-        $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://api.github.com/repos/ste7een/self-updating/releases/latest');
-        $latest = json_decode($res->getBody()->getContents());
+        //on click of button run command line
+        Artisan::call('migrate');
+        $this->info('Migrated.');
+        // Process('git merge main');
+        $this->info('Merged.');
+        return 0;
+        //command to merge the main branch
+        // $client = new \GuzzleHttp\Client();
+        // $res = $client->request('GET', 'https://api.github.com/repos/ste7een/self-updating/releases/latest');
+        // $latest = json_decode($res->getBody()->getContents());
 
-        // get current release from config
-        $current = config('app.version');
+        // // get current release from config
+        // $current = config('app.version');
 
-        if ($latest->tag_name > $current) {
-            Artisan::call('down');
-            Artisan::call('up');
-            $this->info('New update available. Please restart your application.');
-        } else {
-            $this->info('No new updates available.');
-        }
+        // if ($latest->tag_name > $current) {
+        //     Artisan::call('down');
+        //     Artisan::call('up');
+        //     $this->info('New update available. Please restart your application.');
+        // } else {
+        //     $this->info('No new updates available.');
+        // }
     }
 }
